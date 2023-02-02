1b797973061a5eed4ba71f5636be1627016c1132
vmhowley <vmhowleyh@gmail.com>
2023-02-02 16:44:03 -0400

    v1.0.0

--- foodApp/www/assets/js/appMain.js
+++ foodApp/www/assets/js/appMain.js
@@ -15,72 +15,17 @@
     measurementId: "G-K2TXB7V7BJ"
   };
    const app = initializeApp(firebaseConfig) 
-
-
-
   //lee datos de empresa
-const db = getDatabase(app);
-
+  const db = getDatabase(app);
 const empcount = ref(db, 'main_home' )
 onValue(empcount, (snapshot) => {
-  var empCountval = snapshot.val()
-  })
-
-  for (let i = 0; i < empCountval.length ; i++) {
-
-const empname = ref(db, 'main_home/' + i  + '/' + '/nombre');
-onValue(empname, (snapshot) => {
-  const empName = snapshot.val();
-  const empname = document.querySelector('#empName');
-  empname.innerHTML = empName;
+const empCountval = snapshot.val()
   
-  const empid = ref(db, 'main_home/' + i  + '/' + '/id');
-  onValue(empid, (snapshot) => {
-      const empId = snapshot.val();
-      const linkHref = document.querySelector('#linkHref')
-      linkHref.setAttribute("href", "single-"+empId+".html");
-      empname.setAttribute("href", "single-"+empId+".html");
+for (let i = 1; i < empCountval.length ; i++) {
 
-    const ratingref = ref(db, 'main_home/' + i  + '/' + '/rating');
-    onValue(ratingref, (snapshot) => {
-      const emprating = snapshot.val();
-      const empRating = document.querySelector('#empRating');
-      empRating.innerHTML = emprating;
-      
-      const empaddress = ref(db, 'main_home/' + i  + '/' + '/address');
-      onValue(empaddress, (snapshot) => {
-        const empaddress = snapshot.val();
-        const empAddress = document.querySelector('#empAddress');
-        empAddress.innerHTML = empaddress;
-
-        const empstatus = ref(db, 'main_home/' + i  + '/' + '/status');
-        onValue(empstatus, (snapshot) => {
-          const empstatus = snapshot.val();
-          const empStatus = document.querySelector('#empStatus');
-          if (empstatus == "close"){
-            empStatus.setAttribute("style","color:red");
-          }else{
-            empStatus.setAttribute("style","color:green");
-
-          }
-          empStatus.innerHTML = empstatus;
-    
-
-         const empimg =  ref(db, 'main_home/' + i  + '/' + '/profile_picture');
-         onValue(empimg, (snapshot) => {
-           const data = snapshot.val();
-           const empimg = document.querySelector('#empImg');
-           empimg.setAttribute("src",data);    
   
- 
-  });
-  });
-  });
-  });
-  });
 
-
-  const newcontent = elemntFromHtml(`
+ const newcontent = elemntFromHtml(`
   <div class="item">
   <div class="single-product-wrap">
       <div class="thumb">
@@ -103,15 +48,76 @@ onValue(empname, (snapshot) => {
   </div>
   `)
 
+
+  
+
+  
+const empname = ref(db, 'main_home/' + i  + '/' + '/nombre')
+onValue(empname, (snapshot) => {
+  const empName = snapshot.val()
+  const empname = document.querySelector('#empName')
+  empname.innerHTML = empName
 });
-   } 
+
+  const empid = ref(db, 'main_home/' + i  + '/' + '/id')
+  onValue(empid, (snapshot) => {
+      const empId = snapshot.val()
+      const linkHref = document.querySelector('#linkHref')
+      linkHref.setAttribute("href", "single-"+empId+".html")
+      empname.setAttribute("href", "single-"+empId+".html")
+  });
+
+    const ratingref = ref(db, 'main_home/' + i  + '/' + '/rating')
+    onValue(ratingref, (snapshot) => {
+      const emprating = snapshot.val()
+      const empRating = document.querySelector('#empRating')
+      empRating.innerHTML = emprating
+    });
+
+      const empaddress = ref(db, 'main_home/' + i  + '/' + '/address')
+      onValue(empaddress, (snapshot) => {
+        const empaddress = snapshot.val()
+        const empAddress = document.querySelector('#empAddress')
+        empAddress.innerHTML = empaddress
+      });
+
+        const empstatus = ref(db, 'main_home/' + i  + '/' + '/status')
+        onValue(empstatus, (snapshot) => {
+          const empstatus = snapshot.val()
+          const empStatus = document.querySelector('#empStatus')
+          if (empstatus == "close"){
+            empStatus.setAttribute("style","color:red")
+          }else{
+            empStatus.setAttribute("style","color:green")
+
+          }
+          empStatus.innerHTML = empstatus
+        });
+
+
+         const empimg =  ref(db, 'main_home/' + i  + '/' + '/profile_picture')
+         onValue(empimg, (snapshot) => {
+           const data = snapshot.val()
+           const empimg = document.querySelector('#empImg')
+           empimg.setAttribute("src",data)   
+  
+          
+          });
+
+          const parentDiv = document.getElementById("div1").parentNode;   
+          const vp2 = document.getElementById("div1");  
+          document.body.appendChild(newcontent)
+          parentDiv.insertBefore(newcontent, vp2)
+        }
+
+      })
+      
+ 
+
+
   function elemntFromHtml(html){
     const template = document.createElement("template");   
     template.innerHTML = html.trim();
     return template.content.firstElementChild;
+    
   }
-const parentDiv = document.getElementById("div1").parentNode;   
-const vp2 = document.getElementById("div1");  
-document.body.appendChild(newcontent)
-parentDiv.insertBefore(newcontent, vp2)
-
