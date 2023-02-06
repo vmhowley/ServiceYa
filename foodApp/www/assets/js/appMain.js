0533719661ac1cc87a2b5bc4ab7f2d9d5a2b709d
vmhowley <vmhowleyh@gmail.com>
2023-02-06 08:07:54 -0400

    v1.0.0

--- foodApp/www/assets/js/appMain.js
+++ foodApp/www/assets/js/appMain.js
@@ -1,88 +1,63 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js"
-  import { getDatabase, ref, onValue }  from  "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js"
-  import { app } from "./firebase.js"
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  import { getDatabase, ref, onValue }  from  "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
+    import { app } from "./firebase.js";
 
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyBwtWOyTvSKPFtSRnRpE11OrUvX3bh5y8U",
-  //   authDomain: "deliveryapp-ca6c5.firebaseapp.com",
-  //   databaseURL: "https://deliveryapp-ca6c5-default-rtdb.firebaseio.com",
-  //   projectId: "deliveryapp-ca6c5",
-  //   storageBucket: "deliveryapp-ca6c5.appspot.com",
-  //   messagingSenderId: "478233782508",
-  //   appId: "1:478233782508:web:ed7c52894a7ba9922c5dc9",
-  //   measurementId: "G-K2TXB7V7BJ"
-  // };
-  //  const app = initializeApp(firebaseConfig) 
-  //lee datos de empresa
-  const db = getDatabase(app);
-const empcount = ref(db, 'main_home' )
+
+
+const db = getDatabase(app);
+var empcount = ref(db, 'main_home' )
 onValue(empcount, (snapshot) => {
-const empCountval = snapshot.val()
+var empCountval = snapshot.val()
+
+
 var empCountlen = empCountval.length 
-  
-for (let i = 0; i < empCountlen ; i++) {
 
 
- const newcontent = elemntFromHtml(`
- <div id="div3" ></div>
- <div  class="home-product-slider owl-carousel">
-                <div class="item">
-                    <div class="single-product-wrap">
-                        <div class="thumb">
-                            <span class="tag">15% Off</span>
-                            <a id="linkHref" href="single-restuarent.html">
-                                <img  id="empImg" src="" alt="img">
-                            </a>
-                            <a class="fav-btn" href="#"><i class="fa fa-heart"></i></a>
-                        </div>
-                        <div class="details">
-                            <h6><a id="empName" href="single-restuarent.html"></a> <span id="empStatus" style="color:red" ></span></h6>
-                            <div class="ratting">
-                                <i id="empRating" class="ri-star-fill ps-0"></i>
-                                <span id="plpRating" >(6k)</span>
-                                <span id="empAddress" >Av.Sarazora 102</span>
-                            </div>
-                            <span>20-25 Min <span class="ms-3">Free Delivery</span></span>
-                        </div>
+ // const parentDiv = document.getElementById("div0").parentNode
+  //const vp2 = document.getElementById("div0");
+ // document.body.appendChild(newcontent)
+  for (let i = 0; i < empCountlen ; i++) {
+
+    var newcontent = `
+    <div class="item">
+    <div class="single-product-wrap">
+        <div class="thumb">
+            <span class="tag">15% Off</span>
+            <a id="linkHref" href="single-restuarent.html">
+                <img  id="empImg`+i+`" src="" alt="img">
+            </a>
+            <a class="fav-btn" href="#"><i class="fa fa-heart"></i></a>
+        </div>
+        <div class="details">
+            <h6><a id="empName`+i+`" href="single-restuarent.html"></a> <span id="empStatus" style="color:red" ></span></h6>
+            <div class="ratting`+i+`">
+                <i id="empRating`+i+`" class="ri-star-fill ps-0"></i>
+                <span id="empRatingPeopleCount`+i+`" >(6k)</span>
+                <span id="empAddress`+i+`" >Av.Sarazora 102</span>
             </div>
-  `)
-  
-  if(i == 0){
-    const parentDiv = document.getElementById("div0").parentNode
-    const vp2 = document.getElementById("div0")
-    document.body.appendChild(newcontent)
-    parentDiv.insertBefore(newcontent, vp2)
-      } else {
-        const parentDiv = document.getElementById("div0").parentNode
-        const vp2 = document.getElementById("div"+i)
-        document.body.appendChild(newcontent)
-        parentDiv.insertBefore(newcontent, vp2)
-    
-      }
-    function elemntFromHtml(html){
-    const template = document.createElement("template");   
-    template.innerHTML = html.trim();
-    return template.content.firstElementChild;
+            <span>20-25 Min <span class="ms-3">Free Delivery</span></span>
+        </div>
+    </div>
+    </div>
+    `
 
-    
-  }
+
+//parentDiv.insertBefore(newcontent, vp2)
+ document.getElementById("div0").innerHTML += newcontent;
 
 
 const empname = ref(db, 'main_home/' + i  + '/' + '/nombre')
 onValue(empname, (snapshot) => {
   const empName = snapshot.val()
-  const empname = document.querySelector('#empName')
+  const empname = document.querySelector('#empName'+i)
   empname.innerHTML = empName
 });
 
   const empid = ref(db, 'main_home/' + i  + '/' + '/id')
   onValue(empid, (snapshot) => {
       const empId = snapshot.val()
-      const linkHref = document.querySelector('#linkHref')
-      const empname = document.querySelector('#empName')
+      const linkHref = document.querySelector('#linkHref'+i)
+      const empname = document.querySelector('#empName'+i)
 
       linkHref.setAttribute("href", "single-"+empId+".html")
       empname.setAttribute("href", "single-"+empId+".html")
@@ -91,28 +66,21 @@ onValue(empname, (snapshot) => {
     const ratingref = ref(db, 'main_home/' + i  + '/' + '/rating')
     onValue(ratingref, (snapshot) => {
       const emprating = snapshot.val()
-      const empRating = document.querySelector('#empRating')
+      const empRating = document.querySelector('#empRating'+i)
       empRating.innerHTML = emprating
     });
 
-    const plprating = ref(db, 'main_home/' + i  + '/' + '/plprating')
-    onValue(plprating, (snapshot) => {
-      const plprating = snapshot.val()
-      const plpRating = document.querySelector('#plpRating')
-      plpRating.innerHTML = plprating
-    });
-    
       const empaddress = ref(db, 'main_home/' + i  + '/' + '/address')
       onValue(empaddress, (snapshot) => {
         const empaddress = snapshot.val()
-        const empAddress = document.querySelector('#empAddress')
+        const empAddress = document.querySelector('#empAddress'+i)
         empAddress.innerHTML = empaddress
       });
 
         const empstatus = ref(db, 'main_home/' + i  + '/' + '/status')
         onValue(empstatus, (snapshot) => {
           const empstatus = snapshot.val()
-          const empStatus = document.querySelector('#empStatus')
+          const empStatus = document.querySelector('#empStatus'+i)
           if (empstatus == "close"){
             empStatus.setAttribute("style","color:red")
           }else{
@@ -126,7 +94,7 @@ onValue(empname, (snapshot) => {
          const empimg =  ref(db, 'main_home/' + i  + '/' + '/profile_picture')
          onValue(empimg, (snapshot) => {
            const data = snapshot.val()
-           const empimg = document.querySelector('#empImg')
+           const empimg = document.querySelector('#empImg'+i)
            empimg.setAttribute("src",data)   
   
           
@@ -135,10 +103,17 @@ onValue(empname, (snapshot) => {
           
 
         }
+      });
 
-      })
+      
       
  
 
 
-  
+  // function elemntFromHtml(html){
+  //   const template = document.createElement("template");   
+  //   template.innerHTML = html.trim();
+  //   return template.content.firstElementChild;
+
+    
+  // }
